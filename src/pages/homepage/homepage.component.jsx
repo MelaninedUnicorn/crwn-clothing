a6f95f265ee9689a017893ff3ea06f90b7f7cbe9
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-09 15:49:03 +0100

    brolen homepage into smaller components, finished styling the menu

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import './homepage.styles.scss';
+
+import React, { Component } from 'react'
+
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+   <Directory />
+   
+    </div>
+)
+
+export default HomePage;
