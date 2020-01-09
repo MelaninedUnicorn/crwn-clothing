1ae432e79ac006e5a86216c94a70c6daa06c1448
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-09 21:45:07 +0100

    sign in and sign up page , sign up component , form input component and custom button component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import React from 'react';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <Switch>
     <Route exact path='/' component={HomePage} />
     <Route exact path='/shop' component={ShopPage} />
+    <Route exact path='/sign-in' component={SignInAndSignUpPage} />
     </Switch>
    
     </div>
