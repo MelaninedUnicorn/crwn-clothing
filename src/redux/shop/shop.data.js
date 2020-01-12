242fb6377ad1dbfa4e5ff04436c282a8e9a79e64
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-12 17:18:21 +0100

    data normalization

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,5 +1,5 @@
 const SHOP_DATA = {
-  {
+  hats: {
     id: 1,
     title: "Hats",
     routeName: "hats",
@@ -67,7 +67,7 @@ const SHOP_DATA = {
       }
     ]
   },
-  {
+  sneakers:{
     id: 2,
     title: "Sneakers",
     routeName: "sneakers",
@@ -123,7 +123,7 @@ const SHOP_DATA = {
       }
     ]
   },
-  {
+  jackets:{
     id: 3,
     title: "Jackets",
     routeName: "jackets",
@@ -172,7 +172,7 @@ const SHOP_DATA = {
       }
     ]
   },
-  {
+  womens:{
     id: 4,
     title: "Womens",
     routeName: "womens",
@@ -236,7 +236,7 @@ const SHOP_DATA = {
       }
     ]
   },
-  {
+  mens:{
     id: 5,
     title: "Mens",
     routeName: "mens",
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,7 +17,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector([selectCollections], 
     collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    collections[collectionUrlParam]
   );
