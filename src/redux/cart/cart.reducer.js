269334f4eac109fe41ea8ed500b34f415a699075
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-12 14:07:44 +0100

    added add item, remove item and clear item functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
+import { addItemToCart, removeItemToCart } from "./cart.utils";
+
 import CartActionsTypes from "./cart.types";
-import {addItemToCart} from './cart.utils'
 
 const INITIAL_STATE = {
   hidden: true,
@@ -11,7 +12,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionsTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
     case CartActionsTypes.ADD_ITEM:
-      return { ...state, cartItems:addItemToCart(state.cartItems,action.payload) };
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionsTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionsTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
