2443919935ac0a01787b22284089fcba23c149ee
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-12 19:38:41 +0100

    updating stripe integration and updating favison

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,8 +5,9 @@ import {
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import React from "react";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -29,8 +30,18 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => <CheckoutItem id={cartItem.id} cartItem={cartItem} />)}
-    <div className="total"><span>TOTAL : {total} $</span></div>
+    {cartItems.map(cartItem => (
+      <CheckoutItem id={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">
+      <span>TOTAL : {total} $</span>
+    </div>
+    <div className="test-warning">
+      *Please use the following credit card for payments*
+      <br />
+      4242 4242 4242 4242 -Exp : 01/20 - CVV : 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
